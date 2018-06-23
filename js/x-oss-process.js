58b1a9d778e7950cd92f7802ed2d6814417096bd
twesix <278227739@qq.com>
2018-06-23 17:19:37 +0800

    config

--- js/x-oss-process.js
+++ js/x-oss-process.js
@@ -2,5 +2,8 @@ var imgs = document.getElementsByTagName('img');
 
 for(var i = 0 ; i < imgs.length ; i ++ )
 {
-    imgs[i].src += '?x-oss-process=style/normal';
+    var img = imgs[i]
+    img.setAttribute('data-src', img.src + '?x-oss-process=style/normal')
+    img.src += '?x-oss-process=style/small'
+    img.classList.add('lazyload')
 }
